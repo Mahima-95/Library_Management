473f1b07e04dd166b84fb569d7dcbfbd1e7cfdff
Mahima-95 <agg.mahima@gmail.com>
2017-04-26 07:19:07 +0530

    fix

---
+++ src/main/java/com/library/constants/QueryConstants.java
@@ -0,0 +1,12 @@
+package com.library.constants;
+
+public interface QueryConstants {
+
+	String ADDBOOKS = "INSERT INTO library(bookId, bookName, bookMessage) VALUES(?,?,?)";
+
+	String UPDATEBOOKS = "UPDATE library SET bookName =?, bookMessage=? WHERE bookId = ?";
+
+	String DELETEBOOKS = "delete from library where bookId=?";
+
+	String GETBOOKS = "select * from library";
+}
