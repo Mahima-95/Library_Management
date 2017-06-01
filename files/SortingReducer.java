487209aa35439f6e0f07146173e31c6e0fb5081e
magrawal1 <magrawal1@xavient.com>
2017-06-01 18:20:33 +0530

    file sorting mapper

---
+++ files/SortingReducer.java
@@ -0,0 +1,20 @@
+package com.hadoop.wordcountexample;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class SortingReducer extends
+		Reducer<Text, LongWritable, Text, NullWritable> {
+
+	@Override
+	protected void reduce(Text key, Iterable<LongWritable> value,
+			Context context) throws IOException, InterruptedException {
+		while (value.iterator().hasNext()) {
+			context.write(key, NullWritable.get());
+		}
+	};
+}
